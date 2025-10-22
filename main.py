made a fix to the normalization logic

main.py
@@ -9,10 +9,10 @@ base_config = {
     "distance_dim": 1,
     "distance_columns": ["distanceToSurface"],
     "data_folder": "Data/x_43_data_36",
-    "low_fi_data_folder": "Data/x_43_low_fi_data_36",
+    "low_fi_data_folder": "Data/x_43_low_fi_72",
     "dimension": 2,
     "lhs_sample": 500000,
-    "num_epochs": 50000,
+    "num_epochs": 2,
     "output_dim": 6,
     "param_columns": ["a2", "a3", "a4"],
     "param_dim": 3,
@@ -34,9 +34,14 @@ hyperparams = {
 all_combinations = list(itertools.product(*hyperparams.values()))
 param_names = list(hyperparams.keys())
 
+np.random.seed(42)
+sample_size = 1
+sampled_indices = np.random.choice(len(all_combinations), size=sample_size, replace=False)
+sampled_combinations = [all_combinations[i] for i in sampled_indices]
+
 os.makedirs("configs", exist_ok=True)
 
-for i, combination in enumerate(all_combinations):
+for i, combination in enumerate(sampled_combinations):
     config = base_config.copy()
     
     for param_name, param_value in zip(param_names, combination):
@@ -49,7 +54,18 @@ for i, combination in enumerate(all_combinations):
         yaml.dump(config, f)
 
 if __name__ == "__main__":
-    for i in range(len(all_combinations)):
-        config_path = f"configs/config_hyperparam_sweep_{i}.yaml"
+    # for i in range(len(sampled_combinations)):
+    #     config_path = f"configs/config_hyperparam_sweep_{i}.yaml"
         
-        surrogate = Surrogate(config_path=config_path)
+    #     surrogate = Surrogate(config_path=config_path)
+    #     surrogate._train()
+
+    surrogate = Surrogate(config_path="configs/Vanilla.yaml")
+    surrogate._train()
+    # surrogate._infer_and_validate(file="Data/ellipse_data/ellipse_data_test3.csv", shape=None)
+
+    # surrogate = Surrogate(config_path="configs/double_hf_small.yaml")
+    # surrogate._infer_and_validate(file="Data/x_43_unseen/x_43_a23.563123125_a311.12131191_a48.365209003.csv", shape=None)
+    # surrogate._infer_and_validate(file="Data/x_43_unseen/x_43_a21.101756927_a324.59262374_a417.89264438.csv", shape=None)
+    # surrogate._infer_and_validate(file="Data/x_43_data_36/x_43_a21.037862747_a320.23181912_a414.13846579.csv", shape=None)
+    # surrogate._infer_and_validate(file="Data/x_43_low_fi_72/x_43_a21.8550107_a316.33293864_a410.25707196_low_fi.csv", shape=None)

src/models/vanilla_model/__init__.py
@@ -4,17 +4,18 @@ from ..MLP import MLP
 
 
 class VanillaDeepONet(nn.Module):
-    def __init__(self, coord_dim, param_dim, hidden_size, num_hidden_layers, out_dim):
+    def __init__(self, coord_dim, param_dim, hidden_size, num_hidden_layers, out_dim, aux=None):
         super().__init__()
         self.hidden_size = hidden_size
         self.out_dim = out_dim
         self.num_hidden_layers = num_hidden_layers
+        self.aux = aux
 
         self.branch = MLP(param_dim, hidden_size * out_dim, hidden_size, num_hidden_layers)
 
         self.trunk = MLP(coord_dim, hidden_size, hidden_size, num_hidden_layers)
 
-    def forward(self, coords, params, sdf):
+    def forward(self, coords, params, sdf, aux=None):
         B, n_pts, _ = coords.shape
         H = self.hidden_size
         O = self.out_dim